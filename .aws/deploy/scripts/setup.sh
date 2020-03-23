4e8d7f86c45dad6a562796afc3d1e4716e6f2225
sk409 <deepstrap353@gmail.com>
2020-03-23 14:25:50 +0900

    dev

--- .aws/deploy/scripts/setup.sh
+++ .aws/deploy/scripts/setup.sh
@@ -7,5 +7,5 @@ export HOME=/root
 /usr/local/bin/composer install
 npm install
 php artisan migrate
-chmod -R 777 storage
-chmod -R 775 bootstrap/cache
+sudo chmod -R 777 storage
+sudo chmod -R 775 bootstrap/cache
